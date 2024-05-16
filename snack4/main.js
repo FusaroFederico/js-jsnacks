aedb598408cff8393d3d02d5d023544012983728
FedeFus <fusarofede.15@gmail.com>
2024-05-16 20:53:36 +0200

    end of snack4

---
+++ snack4/main.js
@@ -0,0 +1,17 @@
+('use strict');
+
+// lista invitati
+const listaInvitati = ['Marco', 'Giuseppe', 'Luigi', 'Samuele'];
+
+// chiede all'utente di inserise il suo nome
+const nomeUtente = prompt('inserisci il tuo nome:');
+
+// verifica se il nome è presente sulla lista
+for (let i = 0; i < listaInvitati.length; i++) {
+    let nomeLista = listaInvitati[i];
+    if (nomeLista === nomeUtente) {
+        console.log('puoi entrare');
+    } else if (i === listaInvitati.length) {
+        console.log('mi dispiace il tuo nome non è sulla lista. Non puoi entare');
+    }
+}
